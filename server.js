22bfe4f05636240014fa48cf3e31941e8a459ae7
ceress <markmesina69@gmail.com>
2020-03-26 12:39:52 -0700

    boilerplate code

---
+++ server.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employee_db"
+  });
+  
+  connection.connect(function (err) {
+    if (err) throw err;
+    runSearch();
+  });
+  
